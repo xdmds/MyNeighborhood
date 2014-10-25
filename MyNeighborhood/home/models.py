26fcbbca90c1fe7213bc1a30ba5f31548e385db1
LukeHarrold <lvh3851@g.rit.edu>
2014-10-24 22:10:57 -0400

    Fixed the errors in models.py, and added more documentation

--- MyNeighborhood/home/models.py
+++ MyNeighborhood/home/models.py
@@ -1,17 +1,31 @@
 from django.db import models
 
-class Complaint(model.Model):
-	address=models.CharField(max_length=50)
-	date_time=models.DatetimeField()
-	#zip_code=models.SmallIntegerField()
-	complaint_type=models.ForeignKey(Grouping)
+class Location(models.Model):
+	"""
+	Location is the higest level of division, and is the 
+	first sorting the user completes
+	"""
+	zip_code=models.SmallIntegerField()
 
-
-class Grouping(model.Model):
-	complaint_type=models.CharField(max_length=20)
+class Complaint_type(models.Model):
+	"""
+	Complaint_type represents the grouping of complaints by type,
+	specific to the location
+	"""
+	category=models.CharField(max_length=20)
 	num_comps=models.SmallIntegerField()
 	location=models.ForeignKey(Location)
 
-class Location(model.Model):
-	zip_code=models.SmallIntgerField()
+class Complaint(models.Model):
+	"""
+	Complaint is the leaf of the graph, and holds the
+	incident-specific data
+	"""
+	address=models.CharField(max_length=50)
+	date_time=models.DateTimeField()
+	complaint_type=models.ForeignKey(Complaint_type)
+	status=models.CharField(max_length=20)
+	x_coor=models.IntegerField()
+	y_coor=models.IntegerField()
+
 
