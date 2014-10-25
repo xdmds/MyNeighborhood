8e1e2861d0467f7cfe2d0a3423a8fad240fa2d0a
unknown <xdmds200092@gmail.com>
2014-10-24 23:43:37 -0400

    wrote get_complaints()

--- MyNeighborhood/home/views.py
+++ MyNeighborhood/home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+"""
+get the complaints for the filter options selected
+"""
+def get_complaints(zipcode, complaint_type, address, date_created, latitude, longitude, status){
+	complaints = requests.get('http://data.cityofnewyork.us/resource/erm2-nwe9.json?'
+						+ 'incident_zip=' + zipcode
+						+ 'complaint_type=' + complaint_type
+						+ 'incident Address=' + address
+						+ 'created_date=' + date_created
+						+ 'latitude=' + latitude
+						+ 'longitude=' + longitude
+						+ 'status=' + status
+					)
+	return complaints
+}
