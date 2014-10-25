4cec1788644047d2a3604c4d71d5013b0590f5bd
unknown <xdmds200092@gmail.com>
2014-10-25 01:03:43 -0400

    implemented complaints() and complaint_types() in views.py

--- MyNeighborhood/home/views.py
+++ MyNeighborhood/home/views.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render
 import requests
+import json
 
 # Create your views here.
 """
 get the complaints for the filter options selected
 """
-def get_complaints(zipcode, complaint_type, address, date_created, latitude, longitude, status){
-	complaints = requests.get('http://data.cityofnewyork.us/resource/erm2-nwe9.json?'
-						+ 'incident_zip=' + zipcode
-						+ 'complaint_type=' + complaint_type
-						+ 'incident Address=' + address
-						+ 'created_date=' + date_created
-						+ 'latitude=' + latitude
-						+ 'longitude=' + longitude
-						+ 'status=' + status
-					)
-	return complaints
-}
+def complaints(zipcode, complaint_type):
+	print (zipcode)
+	print (complaint_type)
+	string = 'http://data.cityofnewyork.us/resource/erm2-nwe9.json?'+ '$select=latitude,longitude&incident_zip=' + zipcode + '&complaint_type=' + complaint_type
+	complaints = requests.get(string).json()
+	print (complaints)
+
+	complaint_list = []
+	for c in complaints:
+		if 'latitude' in c and 'longitude' in c:
+			complaint_list.append( (c['latitude'], c['longitude']) )
+
+	return complaint_list
+
+"""
+returns the unique complaint types for the 1000 issues pulled from SODA API with no filters or queries
+"""
+def complaint_types():
+	type_set = set()
+	complaints = requests.get('http://data.cityofnewyork.us/resource/erm2-nwe9.json?$select=complaint_type').json()
+
+	for c_type in complaints:
+		type_set.add(c_type['complaint_type'])
+
+	return type_set
